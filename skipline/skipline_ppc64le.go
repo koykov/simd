84a1290d04f07b16851c91f94d71714e7a1e2b81
Konstantin Voykov <koykov0@gmail.com>
2025-05-27 22:37:49 +0300

    skipline: add support of ppc64le arch

---
+++ skipline/skipline_ppc64le.go
@@ -0,0 +1,20 @@
+package skipline
+
+import "golang.org/x/sys/cpu"
+
+var funcPPC64LE func([]byte) int
+
+func init() {
+	if cpu.PPC64.HasVMX && cpu.PPC64.HasVSX {
+		funcPPC64LE = funcPPC64LE
+		return
+	}
+	funcPPC64LE = skiplineGeneric
+}
+
+func skipline(b []byte) int {
+	return funcPPC64LE(b)
+}
+
+//go:noescape
+func skiplinePPC64LE(b []byte) int
