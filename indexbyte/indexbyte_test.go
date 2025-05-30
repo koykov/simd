ac49f9f9e217477fd16fd0c8bbb4400caf7d5653
Konstantin Voykov <koykov0@gmail.com>
2025-05-30 20:42:19 +0300

    indexbyte: add index64 test cases

--- indexbyte/indexbyte_test.go
+++ indexbyte/indexbyte_test.go
@@ -10,13 +10,21 @@ type stage struct {
 	pos  int
 }
 
-var stages []stage
+var (
+	stages   []stage
+	stages64 []stage
+)
 
 func init() {
 	for i := 1; i < 1e10; i *= 10 {
 		data := make([]byte, i-1, i)
 		data = append(data, 'X')
 		stages = append(stages, stage{data: data, pos: len(data) - 1})
+	}
+	for i := 0; i < 64; i++ {
+		data := make([]byte, 64)
+		data[i] = 'X'
+		stages64 = append(stages64, stage{data: data, pos: i})
 	}
 }
 
@@ -38,6 +46,37 @@ func indextest(b []byte, x byte, fn func([]byte, byte) int) (i int) {
 
 func TestIndex(t *testing.T) {
 	for _, st := range stages {
+		t.Run(strconv.Itoa(len(st.data)), func(t *testing.T) {
+			t.Run("generic", func(t *testing.T) {
+				pos := indexbyteGeneric(st.data, 'X')
+				if pos != st.pos {
+					t.Errorf("got %d, want %d", pos, st.pos)
+				}
+			})
+			t.Run("sse2", func(t *testing.T) {
+				pos := indextest(st.data, 'X', indexbyteSSE2)
+				if pos != st.pos {
+					t.Errorf("got %d, want %d", pos, st.pos)
+				}
+			})
+			t.Run("avx2", func(t *testing.T) {
+				pos := indextest(st.data, 'X', indexbyteAVX2)
+				if pos != st.pos {
+					t.Errorf("got %d, want %d", pos, st.pos)
+				}
+			})
+			t.Run("avx512", func(t *testing.T) {
+				pos := indextest(st.data, 'X', indexbyteAVX512)
+				if pos != st.pos {
+					t.Errorf("got %d, want %d", pos, st.pos)
+				}
+			})
+		})
+	}
+}
+
+func TestIndex64(t *testing.T) {
+	for _, st := range stages64 {
 		t.Run(strconv.Itoa(len(st.data)), func(t *testing.T) {
 			t.Run("generic", func(t *testing.T) {
 				pos := indexbyteGeneric(st.data, 'X')
