696aeace7f2349e9bbeefce5fc7c3e4f5d4a1c2c
Konstantin Voykov <koykov0@gmail.com>
2025-06-03 23:39:41 +0300

    indexbyte: add NE version for arm64 arch

---
+++ indexbyte/indexbyte_ne_arm64.go
@@ -0,0 +1,18 @@
+package indexbyte
+
+var funcneARM64 func([]byte, byte) int
+
+func init() {
+	if cpu.ARM64.HasASIMD {
+		funcneARM64 = indexbyteneNEON
+		return
+	}
+	funcneARM64 = indexbyteneGeneric
+}
+
+func indexbytene(b []byte, x byte) int {
+	return funcneARM64(b, x)
+}
+
+//go:noescape
+func indexbyteneNEON([]uint64, byte)
