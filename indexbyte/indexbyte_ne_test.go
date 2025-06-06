6c6170f3d08e3f6a1aa949dce9676bb7aef9c0df
Konstantin Voykov <koykov0@gmail.com>
2025-06-06 19:05:13 +0300

    indexbyte: cleanup test/bench (NE version)

--- indexbyte/indexbyte_ne_test.go
+++ indexbyte/indexbyte_ne_test.go
@@ -25,112 +25,34 @@ func init() {
 	}
 }
 
-func indexnetest(b []byte, x byte, fn func([]byte, byte) int) (i int) {
-	n := len(b)
-	if n < 128 {
-		return indexbyteneGeneric(b, x)
-	}
-	n64 := n - n%64
-	if i = fn(b[:n64], x); i >= 0 {
-		return i
-	}
-	if i = indexbyteneGeneric(b[n64:], x); i >= 0 {
-		return n64 + i
-	}
-	return -1
-}
-
 func TestIndexNE(t *testing.T) {
 	for _, st := range stages {
 		t.Run(strconv.Itoa(len(st.data)), func(t *testing.T) {
-			t.Run("generic", func(t *testing.T) {
-				pos := indexbyteGeneric(st.data, 'X')
-				if pos != st.pos {
-					t.Errorf("got %d, want %d", pos, st.pos)
-				}
-			})
-			t.Run("sse2", func(t *testing.T) {
-				pos := indexnetest(st.data, 'X', indexbyteneSSE2)
-				if pos != st.pos {
-					t.Errorf("got %d, want %d", pos, st.pos)
-				}
-			})
-			t.Run("avx2", func(t *testing.T) {
-				pos := indexnetest(st.data, 'X', indexbyteneAVX2)
-				if pos != st.pos {
-					t.Errorf("got %d, want %d", pos, st.pos)
-				}
-			})
-			t.Run("avx512", func(t *testing.T) {
-				pos := indexnetest(st.data, 'X', indexbyteneAVX512)
-				if pos != st.pos {
-					t.Errorf("got %d, want %d", pos, st.pos)
-				}
-			})
+			pos := IndexNE(st.data, 'X')
+			if pos != st.pos {
+				t.Errorf("got %d, want %d", pos, st.pos)
+			}
 		})
 	}
 }
 
 func TestIndexNE64(t *testing.T) {
 	for _, st := range stages64 {
-		t.Run("generic", func(t *testing.T) {
-			pos := indexbyteneGeneric(st.data, 'X')
-			if pos != st.pos {
-				t.Errorf("got %d, want %d", pos, st.pos)
-			}
-		})
-		t.Run("sse2", func(t *testing.T) {
-			pos := indexnetest(st.data, 'X', indexbyteneSSE2)
-			if pos != st.pos {
-				t.Errorf("got %d, want %d", pos, st.pos)
-			}
-		})
-		t.Run("avx2", func(t *testing.T) {
-			pos := indexnetest(st.data, 'X', indexbyteneAVX2)
-			if pos != st.pos {
-				t.Errorf("got %d, want %d", pos, st.pos)
-			}
-		})
-		t.Run("avx512", func(t *testing.T) {
-			pos := indexnetest(st.data, 'X', indexbyteneAVX512)
-			if pos != st.pos {
-				t.Errorf("got %d, want %d", pos, st.pos)
-			}
-		})
+		pos := IndexNE(st.data, 'X')
+		if pos != st.pos {
+			t.Errorf("got %d, want %d", pos, st.pos)
+		}
 	}
 }
 
 func BenchmarkIndexNE(b *testing.B) {
 	for _, st := range stages {
 		b.Run(strconv.Itoa(len(st.data)), func(b *testing.B) {
-			b.Run("generic", func(b *testing.B) {
-				b.ReportAllocs()
-				b.SetBytes(int64(len(st.data)))
-				for i := 0; i < b.N; i++ {
-					indexbyteneGeneric(st.data, 'X')
-				}
-			})
-			b.Run("sse2", func(b *testing.B) {
-				b.ReportAllocs()
-				b.SetBytes(int64(len(st.data)))
-				for i := 0; i < b.N; i++ {
-					indexnetest(st.data, 'X', indexbyteneSSE2)
-				}
-			})
-			b.Run("avx2", func(b *testing.B) {
-				b.ReportAllocs()
-				b.SetBytes(int64(len(st.data)))
-				for i := 0; i < b.N; i++ {
-					indexnetest(st.data, 'X', indexbyteneAVX2)
-				}
-			})
-			b.Run("avx512", func(b *testing.B) {
-				b.ReportAllocs()
-				b.SetBytes(int64(len(st.data)))
-				for i := 0; i < b.N; i++ {
-					indexnetest(st.data, 'X', indexbyteneAVX512)
-				}
-			})
+			b.ReportAllocs()
+			b.SetBytes(int64(len(st.data)))
+			for i := 0; i < b.N; i++ {
+				IndexNE(st.data, 'X')
+			}
 		})
 	}
 }
