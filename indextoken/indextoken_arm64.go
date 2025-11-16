f690cb67f84cb1dabe29a509167bb8092a8548b7
Konstantin Voykov <koykov0@gmail.com>
2025-11-16 22:23:16 +0300

    indextoken: implement arm64 version

---
+++ indextoken/indextoken_arm64.go
@@ -0,0 +1,18 @@
+package indextoken
+
+var funcARM64 func([]byte) int
+
+func init() {
+	if cpu.ARM64.HasASIMD {
+		funcARM64 = indextokenNEON
+		return
+	}
+	funcARM64 = indextokenGeneric
+}
+
+func indextoken(b []byte) int {
+	return funcARM64(b, x)
+}
+
+//go:noescape
+func indextokenNEON([]byte)
