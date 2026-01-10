7ff892b4e04ed2b788d31a91b963bc9a00d09563
Konstantin Voykov <koykov0@gmail.com>
2026-01-10 22:31:50 +0300

    bitwise OR: implement bytes version

--- bitwise64/or.go
+++ bitwise64/or.go
@@ -1,9 +1,40 @@
 package bitwise64
 
+import (
+	"unsafe"
+)
+
 func Or(a, b []uint64) {
 	al, bl := len(a), len(b)
 	mn := minI(al, bl)
 	or(a[:mn], b[:mn])
+}
+
+func OrBytes(a, b []byte) {
+	al, bl := len(a), len(b)
+	mn := minI(al, bl)
+	var mn8 int
+	if mn > 8 {
+		mn -= mn % 8
+		mn8 = mn
+		mn /= 8
+		type sh struct {
+			p    uintptr
+			l, c int
+		}
+		ah := sh{p: uintptr(unsafe.Pointer(&a[0])), l: mn, c: mn}
+		a64 := *(*[]uint64)(unsafe.Pointer(&ah))
+		bh := sh{p: uintptr(unsafe.Pointer(&b[0])), l: mn, c: mn}
+		b64 := *(*[]uint64)(unsafe.Pointer(&bh))
+		or(a64, b64)
+	}
+
+	a, b = a[mn8:], b[mn8:]
+	mn = minI(len(a), len(b))
+	var i int
+	for i = 0; i < mn; i++ {
+		a[i] |= b[i]
+	}
 }
 
 func orGeneric(a, b []uint64) {
@@ -21,3 +52,5 @@ func minI(a, b int) int {
 	}
 	return b
 }
+
+var _, _ = Or, OrBytes
