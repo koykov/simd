3e928cd7e4ed0b8673b24ebb99cebf6ca577cdba
Konstantin Voykov <koykov0@gmail.com>
2026-03-07 00:06:12 +0300

    memcpy: add bytes version

--- memcpy64/memcpy64.go
+++ memcpy64/memcpy64.go
@@ -1,5 +1,7 @@
 package memcpy64
 
+// Copy copies contents of src to dst.
+// !CAUTION: length of dst must be greater of equal to length of src. You must ensure that.
 func Copy(dst, src []uint64) {
 	memcpy64(dst, src)
 }
